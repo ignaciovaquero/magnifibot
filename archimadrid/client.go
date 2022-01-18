44c31e8ab81842c022e26622045c55187f6d4e4c
Ignacio Vaquero Guisasola <ivaquero@twilio.com>
2022-01-18 09:01:47 +0100

    Parameterized cache TTL

--- archimadrid/client.go
+++ archimadrid/client.go
@@ -7,10 +7,14 @@ import (
 	"github.com/igvaquero18/magnifibot/utils"
 )
 
-const DefaultURL = "https://www.archimadrid.org/index.php/oracion-y-liturgia/index.php?option=com_archimadrid&format=ajax&task=leer_lecturas"
+const (
+	DefaultURL = "https://www.archimadrid.org/index.php/oracion-y-liturgia/index.php?option=com_archimadrid&format=ajax&task=leer_lecturas"
+	DefaultTTL = 24 * time.Hour
+)
 
 type Client struct {
 	url string
+	ttl time.Duration
 	utils.Logger
 	ttlcache.SimpleCache
 }
@@ -20,16 +24,20 @@ type Option func(c *Client) Option
 
 // NewClient returns a new instance of Client
 func NewClient(opts ...Option) *Client {
-	cache := ttlcache.NewCache()
-	cache.SetTTL(24 * time.Hour) // TODO: Parameterize
 	m := &Client{
-		url:         DefaultURL,
-		Logger:      &utils.DefaultLogger{},
-		SimpleCache: cache,
+		url:    DefaultURL,
+		Logger: &utils.DefaultLogger{},
+		ttl:    DefaultTTL,
 	}
+
 	for _, opt := range opts {
 		opt(m)
 	}
+
+	cache := ttlcache.NewCache()
+	cache.SetTTL(m.ttl)
+	m.SimpleCache = cache
+
 	return m
 }
 
@@ -48,5 +56,14 @@ func SetLogger(logger utils.Logger) Option {
 		prev := c.Logger
 		c.Logger = logger
 		return SetLogger(prev)
+	}
+}
+
+// SetCacheTTL Sets the TTL for the Cache
+func SetCacheTTL(ttl time.Duration) Option {
+	return func(c *Client) Option {
+		prev := c.ttl
+		c.ttl = ttl
+		return SetCacheTTL(prev)
 	}
 }
