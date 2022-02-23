fa6c321ad05a3b31e8aaf78cf2ce8193ed561b1e
Ignacio Vaquero Guisasola <ivaquero@twilio.com>
2022-02-23 09:40:14 +0100

    feat(controller): add GetChatIDs method
    add GetChatIDs method in the controller so that it is easier to use from the GetGospelAndNotify function

--- controller/user.go
+++ controller/user.go
@@ -13,8 +13,8 @@ const (
 	DefaultUserTable = "MagnifibotUser"
 )
 
-func (m *Magnifibot) Suscribe(chatID, userID, date int64, kind string) error {
-	_, err := m.PutItem(context.TODO(), &dynamodb.PutItemInput{
+func (m *Magnifibot) Suscribe(ctx context.Context, chatID, userID, date int64, kind string) error {
+	_, err := m.PutItem(ctx, &dynamodb.PutItemInput{
 		TableName: aws.String(m.Config.UserTable),
 		Item: map[string]types.AttributeValue{
 			"ChatID": &types.AttributeValueMemberN{Value: fmt.Sprintf("%d", chatID)},
@@ -30,13 +30,34 @@ func (m *Magnifibot) Suscribe(chatID, userID, date int64, kind string) error {
 	return nil
 }
 
-func (m *Magnifibot) Unsuscribe(chatID int64) error {
-	if err := m.delete("ChatID", &types.AttributeValueMemberN{Value: fmt.Sprintf("%d", chatID)}, m.Config.UserTable); err != nil {
+func (m *Magnifibot) Unsuscribe(ctx context.Context, chatID int64) error {
+	if err := m.delete(ctx, "ChatID", &types.AttributeValueMemberN{Value: fmt.Sprintf("%d", chatID)}, m.Config.UserTable); err != nil {
 		return fmt.Errorf("error when deleting chat with id %d: %w", chatID, err)
 	}
 	return nil
 }
 
-func (m *Magnifibot) GetChats() ([]int64, error) {
-	return []int64{}, nil
+func (m *Magnifibot) GetChatIDs(ctx context.Context) ([]string, error) {
+	scanOutput, err := m.Scan(ctx, &dynamodb.ScanInput{
+		TableName:            aws.String(m.Config.UserTable),
+		ProjectionExpression: aws.String("ChatID"),
+	})
+	if err != nil {
+		return []string{}, fmt.Errorf("error scanning dynamodb table: %w", err)
+	}
+
+	chatIDs := []string{}
+	for _, item := range scanOutput.Items {
+		if chatID, ok := item["ChatID"]; ok {
+			id, ok := chatID.(*types.AttributeValueMemberN)
+			if !ok {
+				return []string{}, fmt.Errorf("error converting ChatID into a string: %w", err)
+			}
+			chatIDs = append(chatIDs, id.Value)
+		} else {
+			return []string{}, fmt.Errorf("error getting ChatID for item %v: %w", item, err)
+		}
+	}
+
+	return chatIDs, nil
 }
