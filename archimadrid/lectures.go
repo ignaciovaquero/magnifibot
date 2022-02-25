927c7c4d3f2a9c7c44b50e522940e765d2db87a5
Ignacio Vaquero Guisasola <ivaquero@twilio.com>
2022-02-25 07:59:26 +0100

    add skeleton of GetFirstLecture

--- archimadrid/lectures.go
+++ archimadrid/lectures.go
@@ -1,1 +1,15 @@
 package archimadrid
+
+import (
+	"context"
+	"time"
+)
+
+func (c *Client) getLectureFromCache(key string) (string, error) {
+	return "", nil
+}
+
+func (c *Client) GetFirstLecture(ctx context.Context, day time.Time) (string, error) {
+	today := day.Format("2006-01-02")
+	return c.getLectureFromCache("first lecture " + today)
+}
